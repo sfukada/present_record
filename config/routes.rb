cba4c2edd30523252b1d875eba88f146cdb67594
Shiori Fukada <shiori@Shiori-no-MacBook.local>
2017-01-31 11:57:10 +0900

    css(header, index)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'presents#index'
   get 'presents' => 'presents#index'
-  get 'presents/new' => 'presents#new'
-  post 'presents' => 'presents#create'
+  resources :presents
 end
